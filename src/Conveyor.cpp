d657435c6e7e91a2106f4aa51fb4d94a4815974c
Yanis Guaye <yanis.guaye@epitech.eu>
2024-10-06 16:42:27 +0200

    Add logging macros

--- src/Conveyor.cpp
+++ src/Conveyor.cpp
@@ -1,4 +1,5 @@
 #include "TheTranslationConfig.hpp"
+#include "Logger.hpp"
 #include "Conveyor.hpp"
 
 #if CONVEYOR_CURRENT_STATUS_PIN >= 0 || CONVEYOR_DESIRED_STATUS_PIN >= 0
@@ -21,7 +22,15 @@ void Conveyor::begin(TwoWire *wire)
 
 void Conveyor::update()
 {
+    ConveyorStatus oldStatus = m_currentStatus;
     m_currentStatus = m_grbl.readIdle() ? ConveyorStatus::STOPPED : ConveyorStatus::RUNNING;
+
+#if LOG_LEVEL >= LOG_LEVEL_DEBUG
+    if (oldStatus != m_currentStatus)
+    {
+        LOG_DEBUG("[CONV] Changing status: %s\n", CONVEYOR_STATUS_STRINGS[static_cast<int>(m_desiredStatus)]);
+    }
+#endif
 
     if (m_currentStatus == ConveyorStatus::STOPPED && m_desiredStatus == ConveyorStatus::RUNNING)
     {
@@ -44,6 +53,7 @@ Conveyor::Conveyor()
 
 void Conveyor::begin()
 {
+    LOG_DEBUG("[CONV] Initializing...\n");
     m_desiredStatus = ConveyorStatus::STOPPED;
     m_currentStatus = ConveyorStatus::STOPPED;
     m_motorDelay = 0;
@@ -59,6 +69,7 @@ void Conveyor::update()
 {
     if (m_currentStatus != m_desiredStatus && m_motorDelay++ > CONVEYOR_SIMULATED_DELAY_UPDATES)
     {
+        LOG_DEBUG("[CONV] Changing status: %s\n", CONVEYOR_STATUS_STRINGS[static_cast<int>(m_desiredStatus)]);
         m_motorDelay = 0;
         m_currentStatus = m_desiredStatus;
     }
@@ -73,11 +84,13 @@ void Conveyor::update()
 
 void Conveyor::start()
 {
+    LOG_DEBUG("[CONV] Starting...\n");
     m_desiredStatus = ConveyorStatus::RUNNING;
 }
 
 void Conveyor::stop()
 {
+    LOG_DEBUG("[CONV] Stopping...\n");
     m_desiredStatus = ConveyorStatus::STOPPED;
 }
 
