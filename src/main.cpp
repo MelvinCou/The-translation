b4883a032b0b1382dec82d41b7b160cd46251e70
MelvinCou <melvin.courjaud@epitech.eu>
2024-10-06 21:12:08 +0200

    WIP Use AsyncTCP to do HTTP requests

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,18 @@
 ******************************************************************************
 */
 #include <Arduino.h>
-#include <HTTPClient.h>
 #include <M5Stack.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
+#include <AsyncTCP.h>
 
 WiFiMulti wifiMulti;
-HTTPClient http;
+AsyncClient client;
 
-#define SSID
-#define PASSWORD
-#define API_KEY
-#define API_URL "http://.../api/index.php/products?sortfield=t.ref&sortorder=ASC&limit=100"
+#define SSID ""
+#define PASSWORD ""
+#define API_KEY ""
+#define API_URL "http://192.168.165.36:8080"
 
 void setup() {
     M5.begin();        // Init M5Core.  初始化 M5Core
@@ -30,38 +30,49 @@ void setup() {
     wifiMulti.addAP(SSID, PASSWORD);  // Storage wifi configuration information.  存储wifi配置信息
     M5.Lcd.print("\nConnecting Wifi...\n");  // print format output string on
                                              // lcd.  串口格式化输出字符串
+    client.onData([](void *obj, AsyncClient *c, void *data, size_t len) {
+        M5.Lcd.print("Data received: ");
+        M5.Lcd.println((char *)data);
+        M5.Lcd.print("Data received: ");
+        M5.Lcd.println(len);
+        c->close();
+    });
+    client.onConnect([](void *obj, AsyncClient *c) {
+        M5.Lcd.print("Connected\n");
+    });
+    client.onError([](void *obj, AsyncClient *c, int8_t error) {
+        M5.Lcd.print("Error: ");
+        M5.Lcd.println(error);
+    });
+    client.onDisconnect([](void *obj, AsyncClient *c) {
+        M5.Lcd.print("Disconnected\n");
+    });
+    client.onTimeout([](void *obj, AsyncClient *c, uint32_t time) {
+        M5.Lcd.print("Timeout\n");
+    });
+    client.onAck([](void *obj, AsyncClient *c, size_t len, uint32_t time) {
+        M5.Lcd.print("Ack: ");
+        M5.Lcd.println(len);
+    });
 }
 
 void loop() {
     M5.Lcd.setCursor(0, 0);  // Set the cursor at (0,0).  设置光标位于(0,0)处
-    if ((wifiMulti.run() ==
-         WL_CONNECTED)) {  // wait for WiFi connection.  等待连接至wifi
+    if ((wifiMulti.run() == WL_CONNECTED)) {  // wait for WiFi connection.  等待连接至wifi
         M5.Lcd.print("[HTTP] begin...\n");
-        http.begin(API_URL);  // configure traged server and
-                                               // url.  配置被跟踪的服务器和URL
-        http.addHeader("DOLAPIKEY", API_KEY);
+
+
+        client.connect("192.168.165.36", 8080);
 
         M5.Lcd.print("[HTTP] GET...\n");
-        int httpCode = http.GET();  // start connection and send HTTP header.
-                                    // 开始连接服务器并发送HTTP的标头
-        if (httpCode >
-            0) {  // httpCode will be negative on error.  出错时httpCode将为负值
-            M5.Lcd.printf("[HTTP] GET... code: %d\n", httpCode);
+        client.add("GET / HTTP/1.1\r\n", 16, ASYNC_WRITE_FLAG_MORE);
+        client.add("\r\n", 2, ASYNC_WRITE_FLAG_MORE);
+        bool status = client.send();
 
-            if (httpCode ==
-                HTTP_CODE_OK) {  // file found at server.  在服务器上找到文件
-                String payload = http.getString();
-                M5.Lcd.println(payload);  //打印在服务器上读取的文件.  Print
-                                          // files read on the server
-            }
-        } else {
-            M5.Lcd.printf("[HTTP] GET... failed, error: %s\n",
-                          http.errorToString(httpCode).c_str());
-        }
-        http.end();
+        M5.Lcd.print("Yay\n");
     } else {
         M5.Lcd.print("connect failed");
     }
-    delay(10000);
+    delay(1000);
     M5.Lcd.clear();  // clear the screen.  清除屏幕
 }
