00c345212c75f2b8d3a5b9be7ce73e7b7703ab1a
MelvinCou <melvin.courjaud@epitech.eu>
2024-10-31 11:03:10 +0100

    feat: use variables endpoints instead of magic values

--- src/DolibarrClient.cpp
+++ src/DolibarrClient.cpp
@@ -11,12 +11,13 @@ DolibarrClientStatus DolibarrClient::configure(const char* endpoint, const char*
     client.useHTTP10();
 
     String statusEndpoint = String(endpoint);
-    bool _ = statusEndpoint.concat("/status");
+    bool _ = statusEndpoint.concat(DOLIBARR_ENDPOINT_STATUS);
     client.begin(statusEndpoint);
-    client.addHeader("DOLAPIKEY", key);
+    client.addHeader(DOLIBARR_HEADER_APIKEY, key);
 
     int httpCode = client.GET();
 
+    client.end();
     statusEndpoint.clear();
 
     if(httpCode <= 0) {
@@ -30,25 +31,23 @@ DolibarrClientStatus DolibarrClient::configure(const char* endpoint, const char*
         if(tagEndpoint != nullptr) {
             tagEndpoint.clear();
             bool _ = tagEndpoint.concat(endpoint);
-            _ = tagEndpoint.concat("/products?limit=1&mode=1&sqlfilters=(t.barcode%3A%3D%3A'");
+            _ = tagEndpoint.concat(DOLIBARR_ENDPOINT_PRODUCTS);
         } else {
             tagEndpoint = String(endpoint);
-            bool _ = tagEndpoint.concat("/products?limit=1&mode=1&sqlfilters=(t.barcode%3A%3D%3A'");
+            bool _ = tagEndpoint.concat(DOLIBARR_ENDPOINT_PRODUCTS);
         }
         
         if(stockMovementEndpoint != nullptr) {
             stockMovementEndpoint.clear();
             bool _ = stockMovementEndpoint.concat(endpoint);
-            _ = stockMovementEndpoint.concat("/stockmovements");
+            _ = stockMovementEndpoint.concat(DOLIBARR_ENDPOINT_STOCKMOVEMENTS);
         } else {
             stockMovementEndpoint = String(endpoint);
-            bool _ = stockMovementEndpoint.concat("/stockmovements");
+            bool _ = stockMovementEndpoint.concat(DOLIBARR_ENDPOINT_STOCKMOVEMENTS);
         }
 
         this->key = key;
     }
-
-    client.end();
 
     return status;
 }
@@ -57,23 +56,21 @@ DolibarrClientStatus DolibarrClient::getStatus() {
     return status;
 }
 
-int DolibarrClient::sendTag(String barcode) {
+void DolibarrClient::sendTag(const String barcode, int& product, int& warehouse) {
     status = DolibarrClientStatus::SENDING;
 
-    int value = -1;
     String endpoint = String(tagEndpoint);
     bool _ = endpoint.concat(barcode);
-    _ = endpoint.concat("')");
+    _ = endpoint.concat(DOLIBARR_ENDPOINT_PRODUCTS_END);
 
     client.begin(endpoint);
-    client.addHeader("DOLAPIKEY", key);
+    client.addHeader(DOLIBARR_HEADER_APIKEY, key);
 
     int httpCode = client.GET();
     endpoint.clear();
 
     if (httpCode == HTTP_CODE_OK)
     {
-        status = DolibarrClientStatus::READY;
         JsonDocument doc;
         DeserializationError error = deserializeJson(doc, client.getStream());
         client.end();
@@ -81,19 +78,22 @@ int DolibarrClient::sendTag(String barcode) {
         if(error) {
             status = DolibarrClientStatus::ERROR;
             LOG_ERROR("[HTTP] Error deserializing sendTag payload: %s\n", error.c_str());
+            warehouse = DOLIBARR_WAREHOUSE_ERROR;
         } else {
-            value = doc[0]["fk_default_warehouse"].as<int>();
+            product = doc[0]["id"].as<int>();
+            warehouse = doc[0]["fk_default_warehouse"].as<int>();
 
-            LOG_DEBUG("[HTTP] default warehouse of %s: %u\n", barcode.c_str(), value);
+            LOG_DEBUG("[HTTP] barecode %s: id %u ; warehouse %u \n", barcode.c_str(), product, warehouse);
+            status = DolibarrClientStatus::READY;
         }
     } else {
         status = DolibarrClientStatus::ERROR;
         
         LOG_ERROR("[HTTP] Error during sendTag request: %s\n", client.errorToString(httpCode).c_str());
         client.end();
-    }
 
-    return value;
+        warehouse = DOLIBARR_WAREHOUSE_ERROR;
+    }
 }
 
 void DolibarrClient::sendStockMovement(int warehouse_source, int product, int quantity) {
@@ -109,7 +109,7 @@ void DolibarrClient::sendStockMovement(int warehouse_source, int product, int qu
     unsigned int _ = serializeJson(doc, body);
 
     client.begin(stockMovementEndpoint);
-    client.addHeader("DOLAPIKEY", key);
+    client.addHeader(DOLIBARR_HEADER_APIKEY, key);
 
     int httpCode = client.POST(body);
 
