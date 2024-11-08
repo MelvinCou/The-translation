efbec2ae983a5d3da6fa5b55b825e2c796e7f0e6
Yanis Guaye <yanis.guaye@epitech.eu>
2024-11-08 12:14:05 +0100

    Always unlock step motor on undefined state

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 #ifdef ENV_M5STACK
 #include <M5Stack.h>
-#else
-#include <SPI.h>
 #endif  // defined(ENV_M5STACK)
 
 #include "Buttons.hpp"
@@ -31,17 +29,29 @@ void setup() {
   M5.begin();             // Init M5Stack.
   M5.Power.begin();       // Init power
   M5.lcd.setTextSize(2);  // Set the text size to 2.
-  Wire.begin(21, 22);     // Wire init, adding the I2C bus.
-  conveyor.begin(&Wire);
+  Wire.begin(21, 22);
   M5.Lcd.println("= Motor Test =");
   M5.Lcd.println("A: Start B: Status C: Stop");
 #else
   conveyor.begin();
   Serial.flush();
 #endif
-  buttons.begin();
+
+#ifdef HARDWARE_GRBL
   sorter.begin();
+  conveyor.begin(&Wire);
+#else
+  sorter.begin();
+  conveyor.begin();
+#endif
+
+#ifdef HARDWARE_MFRC522_I2C
+  tagReader.begin(&Wire);
+#else
   tagReader.begin();
+#endif
+
+  buttons.begin();
   printStatus();
 
   xTaskCreatePinnedToCore(&readButtons, "readButtons", 4096, nullptr, 8, nullptr, 0);
