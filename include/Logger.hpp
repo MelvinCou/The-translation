90af4a017c20b8db5904a8fc3009f7b47104cdbc
Yanis Guaye <yanis.guaye@epitech.eu>
2024-11-29 13:51:50 +0100

    feat: add level marker to log messages

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -17,31 +17,31 @@
 #define LOG_PRINTF(format, ...) Serial.printf(format, ##__VA_ARGS__)
 
 #if LOG_LEVEL >= LOG_LEVEL_ERROR
-#define LOG_ERROR(format, ...) LOG_PRINTF(format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) LOG_PRINTF("[E] " format, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(format, ...) do {} while(0) 
 #endif // LOG_LEVEL >= LOG_LEVEL_ERROR
 
 #if LOG_LEVEL >= LOG_LEVEL_WARN
-#define LOG_WARN(format, ...) LOG_PRINTF(format, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) LOG_PRINTF("[W] " format, ##__VA_ARGS__)
 #else
 #define LOG_WARN(format, ...) do {} while(0)
 #endif // LOG_LEVEL >= LOG_LEVEL_WARN
 
 #if LOG_LEVEL >= LOG_LEVEL_INFO
-#define LOG_INFO(format, ...) LOG_PRINTF(format, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) LOG_PRINTF("[I] " format, ##__VA_ARGS__)
 #else
 #define LOG_INFO(format, ...) do {} while(0)
 #endif // LOG_LEVEL >= LOG_LEVEL_INFO
 
 #if LOG_LEVEL >= LOG_LEVEL_DEBUG
-#define LOG_DEBUG(format, ...) LOG_PRINTF(format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) LOG_PRINTF("[D] " format, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(format, ...) do {} while(0)
 #endif // LOG_LEVEL >= LOG_LEVEL_DEBUG
 
 #if LOG_LEVEL >= LOG_LEVEL_TRACE
-#define LOG_TRACE(format, ...) LOG_PRINTF( format, ##__VA_ARGS__)
+#define LOG_TRACE(format, ...) LOG_PRINTF("[T] " format, ##__VA_ARGS__)
 #else
 #define LOG_TRACE(format, ...) do {} while(0)
 #endif // LOG_LEVEL >= LOG_LEVEL_TRACE
