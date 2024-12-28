9176de4612f5cfacd19ed3e9d30b90e9ef85b5a3
Yanis Guaye <yanis.guaye@epitech.eu>
2024-12-28 17:00:14 +0100

    fix: silence unused constant warnings

--- include/ActiveModule.hpp
+++ include/ActiveModule.hpp
@@ -3,6 +3,6 @@
 
 enum class ActiveModule { NONE = 0, CONVEYOR, SORTER, TAG_READER, DOLIBARR };
 
-static char const *ACTIVE_MODULES[5] = {"NONE", "CONVEYOR", "SORTER", "TAG_READER", "DOLIBARR"};
+static constexpr char const *ACTIVE_MODULES[5] = {"NONE", "CONVEYOR", "SORTER", "TAG_READER", "DOLIBARR"};
 
 #endif  // !defined(ACTIVE_MODULE_HPP)
