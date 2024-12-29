0d64646abd96ddf761929aadd041df5335c2694f
Yanis Guaye <yanis.guaye@epitech.eu>
2024-12-29 22:01:42 +0100

    feat: write logs to external file in addition to stdout

--- lib/Simulation/src/HardwareSerial.cpp
+++ lib/Simulation/src/HardwareSerial.cpp
@@ -1,5 +1,7 @@
 #include "simulation/HardwareSerial.hpp"
 
+#include <unistd.h>
+
 #include <cstdio>
 
 HardwareSerial Serial;
@@ -8,7 +10,11 @@ void HardwareSerial::begin([[maybe_unused]] unsigned long baud) {
   // TODO: stub
 }
 
+extern int loggerOutputFd;
+
 size_t HardwareSerial::write(uint8_t b) {
+  // write to the log file: see esp_main.cpp
+  [[maybe_unused]] ssize_t ignored = ::write(loggerOutputFd, &b, 1);
   std::fputc(b, stdout);
   return 1;
 }
@@ -17,6 +23,8 @@ size_t HardwareSerial::write(const uint8_t *buffer, size_t size) {
   size_t total = 0;
   while (total < size) {
     size_t to_write = size - total;
+    // write to the log file: see esp_main.cpp
+    [[maybe_unused]] ssize_t ignored = ::write(loggerOutputFd, buffer + total, to_write);
     size_t written = std::fwrite(buffer + total, sizeof(*buffer), to_write, stdout);
     total += written;
     if (written == 0) {
--- lib/Simulation/src/esp_main.cpp
+++ lib/Simulation/src/esp_main.cpp
@@ -1,13 +1,33 @@
+#include <esp_log.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <cstdio>
 #include <cstdlib>
 
 void simulationMain();
 void watchdogMain();
 
+int loggerOutputFd = -1;
+static vprintf_like_t originalLogger = nullptr;
+
+/// @brief Logs to a file in addition to the default logger.
+int __attribute__((format(printf, 1, 0))) logToFile(char const *fmt, va_list args) {
+  va_list argsCopy;
+  va_copy(argsCopy, args);
+  vdprintf(loggerOutputFd, fmt, argsCopy);
+  return originalLogger(fmt, args);
+}
+
 /// @brief Runs the entrypoint of the watchdog or simulation based on the environment variable.
 extern "C" void app_main(void) {
   if (getenv("THE_TRANSLATION_SIMULATED") != nullptr) {
+    loggerOutputFd = open("/tmp/the-translation.log", O_WRONLY | O_CREAT | O_APPEND | O_CLOEXEC, 0644);
+    originalLogger = esp_log_set_vprintf(logToFile);
     simulationMain();
   } else {
+    loggerOutputFd = open("/tmp/the-translation.log", O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644);
+    originalLogger = esp_log_set_vprintf(logToFile);
     watchdogMain();
   }
 }
