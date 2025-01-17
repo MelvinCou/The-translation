461cf3411eb9109ca7a54db1de301895ccd3d37a
MelvinCou <melvin.courjaud@epitech.eu>
2025-01-17 12:13:34 +0100

    feat: add sorter direction strings

--- include/Sorter.hpp
+++ include/Sorter.hpp
@@ -7,6 +7,13 @@ enum class SorterDirection {
   RIGHT,
 };
 
+static constexpr char const *SORTER_DIRECTIONS[] = {
+    "(nothing)",
+    "LEFT",
+    "MIDDLE",
+    "RIGHT",
+};
+
 /// @brief The package sorter, usually driven by a servo motor.
 class Sorter {
  public:
@@ -17,6 +24,7 @@ class Sorter {
   void moveWithSpecificAngle(int angle);
   int getCurrentAngle() const;
   int getDesiredAngle() const;
+  SorterDirection getDesiredDirection() const;
   void setDesiredAngle(SorterDirection direction);
 
  private:
